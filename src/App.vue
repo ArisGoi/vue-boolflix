1893f47e6de85ada32b446ee3ab3255bc01a2776
ArisGoi <aris.goi97@gmail.com>
2021-10-06 14:07:44 +0200

    add style: Header

--- src/App.vue
+++ src/App.vue
@@ -57,12 +57,10 @@ export default {
 </script>
 
 <style lang="scss">
+@import './assets/style.scss';
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,8 +1,13 @@
 <template>
-    <div class="container">
-        <input type="text" v-model="searchText">
-        <button @click="$emit('search', searchText)">Cerca</button>
-    </div>
+    <header>
+        <div class="container-l">
+            <h1>BOOLFLIX</h1>
+            <div>
+                <input type="text" v-model="searchText">
+                <button @click="$emit('search', searchText)">Cerca</button>
+            </div>
+        </div>
+    </header>
 </template>
 
 <script>
@@ -17,5 +22,20 @@ export default {
 </script>
 
 <style scoped lang="scss">
+@import '../assets/variables.scss';
+    header{
+        background-color: $c-bg-1;
+        color: white;
 
+        .container-l{
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+            height: $header-height;
+        }
+        
+        h1{
+            color: $c-main;
+        }
+    }
 </style>
