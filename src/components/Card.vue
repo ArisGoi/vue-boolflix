0ed5a2249aee5c7fc0404b00cfca5ca27146c1bc
ArisGoi <aris.goi97@gmail.com>
2021-10-06 13:37:02 +0200

    add stars value

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -5,14 +5,25 @@
         <h4>{{element.original_title}}{{element.original_name}}</h4>
         <!-- <p>Lingua Originale: {{element.original_language}}</p> -->
         <img width="50px" :src="`https://unpkg.com/language-icons/icons/${element.original_language}.svg`" :alt="`${element.original_language}`">
-        <p>Voto: {{element.vote_average}}</p>
+        <!-- <p>Voto: {{`${getStars(element.vote_average)}`}}</p> -->
+        <p>
+            Voto:
+            <i class="fas fa-star" v-for="(starF, index) in getStars(element.vote_average)" :key="index"></i>
+            <i class="far fa-star" v-for="(starF, index) in 5 - getStars(element.vote_average)" :key="index"></i>
+        </p>
     </div>
 </template>
 
 <script>
 export default {
     name: "Card",
-    props:['element']
+    props:['element'],
+    methods:{
+        getStars(vote10){
+            let vote5 = Math.floor(vote10 * 5 / 10);
+            return vote5
+        }
+    }
 }
 </script>
 
